c5a0bb9940b6b3f0bf36a296049ee4c8fced1f1a
Emanuel Caesar <emanuel.caesar@gmail.com>
2018-01-06 18:13:53 +0100

    source

--- LogicalSircuitBoard/And.Designer.cs
+++ LogicalSircuitBoard/And.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // And
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "And";
+            this.Size = new System.Drawing.Size(130, 50);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
